fe7812052812d04ca7b58e836af93bf09aea82af
alex-ubbcluj <alexandru.gruia1@stud.ubbcluj.ro>
2023-03-13 22:30:56 +0200

    done

---
+++ Simulare/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace Simulare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.YellowGreen;
+            this.button1.Location = new System.Drawing.Point(213, 364);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start sort";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 375;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "SImulare algoritm de sortare";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Simulare/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simulare
+{
+    public partial class Form1 : Form
+    {
+        Button[] b = new Button[9];
+        int[] v = new int[9];
+        int i, j;
+        Random r = new Random();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (i = 1; i <= 7; i++)
+            {
+                v[i] = r.Next(50) + 10;
+                b[i] = new Button();
+                b[i].Width = 35;
+                b[i].Height = 4 * v[i];
+                b[i].Left = i * 50;
+                b[i].Top = 10;
+                b[i].Text = v[i].ToString();
+                b[i].ForeColor = Color.White;
+                b[i].BackColor = Color.Gray;
+                this.Controls.Add(b[i]);
+            }
+            b[8] = new Button();
+            b[8].Width = 1;
+            b[8].Height = 1;
+            b[8].Visible = false;
+            this.Controls.Add(b[8]);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(v[i]>v[j])
+            {
+                int aux = v[i];
+                v[i] = v[j];
+                v[j] = aux;
+                b[i].Text = v[i].ToString();
+                b[j].Text = v[j].ToString();
+                b[i].Height = 4 * v[i];
+                b[j].Height = 4 * v[j];
+            }
+            j++;
+            b[j].BackColor = Color.DarkRed;
+            b[j-1].BackColor = Color.Gray;
+            if (j > 7)
+            {
+                i++;
+                b[i].BackColor = Color.DarkRed;
+                b[i-1].BackColor = Color.Gray;
+                j = i + 1;
+                b[j].BackColor = Color.DarkRed;
+                if (i >= 7) { b[i].BackColor = Color.Gray; timer1.Stop(); }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            i = 1;
+            b[i].BackColor = Color.DarkRed;
+            j = 2;
+            b[j].BackColor = Color.DarkRed;
+            timer1.Start();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
